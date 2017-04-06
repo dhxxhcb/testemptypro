3efbeb31a2b5ed5ca2d18e756c54ea69ce99a4a3
zhuzy <22@ee.com>
2017-04-06 17:29:09 +0800

    修改 开发文档

--- xoa/src/com/xoa/controller/IndexController.java
+++ xoa/src/com/xoa/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.xoa.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -11,23 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.alibaba.fastjson.JSON;
 import com.xoa.model.users.Users;
 import com.xoa.service.users.UsersService;
-import com.xoa.service.worldnews.NewService;
 import com.xoa.util.ToJson;
-
-
 
 
 @Controller
 @Scope(value="prototype")
 public class IndexController {
 	private Logger loger = Logger.getLogger(IndexController.class);
-//	@Resource
-//	private UserService userService;
 	@Resource
 	private UsersService usersService;
 	
