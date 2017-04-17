032963136c1bdf6e20468053cc7e09cf22e06e32
zhangyong <349101912@qq.com>
2017-04-17 17:42:48 +0800

    邮箱视图层注释

--- xoa/src/com/xoa/controller/email/EmailUtilController.java
+++ xoa/src/com/xoa/controller/email/EmailUtilController.java
@@ -1,18 +1,17 @@
 package com.xoa.controller.email;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,10 +26,12 @@ import com.xoa.util.DateFormat;
 import com.xoa.util.ToJson;
 
 /**
- * 邮件
  * 
- * @author gaosubo
- * @version 1.0
+ * @ClassName (类名):  EmailUtilController
+ * @Description(简述): 邮件
+ * @author(作者):      zy
+ * @date(日期):        2017-4-17 下午5:34:30
+ *
  */
 @Controller
 @Scope(value = "prototype")
@@ -42,77 +43,107 @@ public class EmailUtilController {
 	private EmailUtilService emailService;
 
 	/**
-	 * 发送邮件并保存
+	 * 
+	 * @Title: insertEmailBody
+	 * @Description: 发送邮件并保存
+	 * @author(作者): zy
+	 * @param: @param fromId 发件人USER_ID,如(lisi,zhangsan,)
+	 * @param: @param toId2 收件人 USER_ID串，每个ID后带一个逗号，如：(lisi,zhangsan,)
+	 * @param: @param copyToId 抄送人USER_ID串,如(lisi,zhangsan,)
+	 * @param: @param subject 邮件主题
+	 * @param: @param content 邮件内容
+	 * @param: @param sendTime 发送时间,如：2017-04-04 10:20:35
+	 * @param: @param attachmentName 附件文件名串
+	 * @param: @param secretToId 密送人USER_ID串,如(lisi,zhangsan,)
+	 * @param: @param attachmentId 附件ID串
+	 * @param: @param sendFlag 是否已发送(0-未发送,1-已发送)
+	 * @param: @param smsRemind 是否使用短信提醒(0-不提醒,1-提醒)
+	 * @param: @param important 重要程度(空-一般邮件,1-重要,2-非常重要)
+	 * @param: @param size 邮件大小
+	 * @param: @param fromWebmailId 从自己的哪个外部邮箱ID对应emailbox中id
+	 * @param: @param fromWebmail 从自己的哪个外部邮箱向外发送
+	 * @param: @param toWebmail 外部收件人邮箱串
+	 * @param: @param compressContent 压缩后的邮件内容
+	 * @param: @param webmailContent 外部邮件内容
+	 * @param: @param webmailFlag 外部邮件标记(0-未发送,1-正在准备发送,2-发送成功,3-发送失败)
+	 * @param: @param recvFromName 接收外部邮箱名称
+	 * @param: @param recvFrom 接收外部邮箱ID
+	 * @param: @param recvToId 发送外部邮件ID
+	 * @param: @param recvTo 发送外部邮箱名称
+	 * @param: @param isWebmail 是否为外部邮件(0-内部邮件,1-外部邮件)
+	 * @param: @param isWf 是否同时外发(0-不外发,1-勾选向此人发送外部邮件)
+	 * @param: @param keyword 内容关键词
+	 * @param: @param secretLevel 邮件密级等级
+	 * @param: @param auditMan 审核人USER_ID
+	 * @param: @param auditRemark 审核不通过备注
+	 * @param: @param copyToWebmail 抄送外部邮箱串
+	 * @param: @param secretToWebmail 抄送外部邮箱串
+	 * @param: @param praise 点赞人user_id串
+	 * @param: @return
+	 * @return: String Json串
+	 * @throws
 	 */
 	@RequestMapping(value = "/sendEmail", produces = { "application/json;charset=UTF-8" })
 	public @ResponseBody
-	String insertEmailBody(@RequestParam(value = "fromId") String fromId,
-			@RequestParam("toId2") String toId2,@RequestParam(value = "copyToId") String copyToId,
-			@RequestParam("secretToId") String secretToId,@RequestParam("subject") String subject,
-			@RequestParam("content") String content,@RequestParam("sendTime") String sendTime,
-			@RequestParam("attachmentId") String attachmentId,@RequestParam("attachmentName") String attachmentName,
-			@RequestParam("sendFlag") String sendFlag,@RequestParam("smsRemind") String smsRemind,
-			@RequestParam("important") String important,@RequestParam("size") String size,
-			@RequestParam("fromWebmailId") String fromWebmailId,@RequestParam("fromWebmail") String fromWebmail,
-			@RequestParam("toWebmail") String toWebmail,@RequestParam("compressContent") String compressContent,
-			@RequestParam("webmailContent") String webmailContent,@RequestParam("webmailFlag") String webmailFlag,
-			@RequestParam("recvFromName") String recvFromName,@RequestParam("recvFrom") String recvFrom,
-			@RequestParam("recvToId") String recvToId,@RequestParam("recvTo") String recvTo,
-			@RequestParam("isWebmail") String isWebmail,@RequestParam("isWf") String isWf,
-			@RequestParam("keyword") String keyword,@RequestParam("secretLevel") String secretLevel,
-			@RequestParam("auditMan") String auditMan,@RequestParam("auditRemark") String auditRemark,
-			@RequestParam("copyToWebmail") String copyToWebmail,@RequestParam("secretToWebmail") String secretToWebmail,
-			@RequestParam("praise") String praise) {
-		EmailBody emailBody = new EmailBody();
-		emailBody.setFromId(this.returnValue(fromId));
-		emailBody.setToId2(toId2);
-		emailBody.setCopyToId(this.returnValue(copyToId));
-		emailBody.setSecretToId(this.returnValue(secretToId));
-		emailBody.setSubject(this.returnValue(subject));
-		emailBody.setContent(this.returnValue(content));
-		emailBody.setSendTime(DateFormat.getTime(sendTime));
-		emailBody.setAttachmentId(this.returnValue(attachmentId));
-		emailBody.setAttachmentName(this.returnValue(attachmentName));
-		emailBody.setSendFlag(this.returnValue(sendFlag));
-		emailBody.setSmsRemind(this.returnValue(smsRemind));
-		emailBody.setImportant(this.returnValue(important));
-		emailBody.setSize(Long.valueOf(size));
-		emailBody.setFromWebmailId(Integer.valueOf(fromWebmailId));
-		emailBody.setFromWebmail(this.returnValue(fromWebmail));
-		emailBody.setToWebmail(this.returnValue(toWebmail));
-		emailBody.setCompressContent(compressContent.getBytes());
-		emailBody.setWebmailContent(webmailContent.getBytes());
-		emailBody.setWebmailFlag(this.returnValue(webmailFlag));
-		emailBody.setRecvFromName(this.returnValue(recvFromName));
-		emailBody.setRecvFrom(this.returnValue(recvFrom));
-		emailBody.setRecvToId(Integer.valueOf(recvToId));
-		emailBody.setRecvTo(this.returnValue(recvTo));
-		emailBody.setIsWebmail(this.returnValue(isWebmail));
-		emailBody.setIsWf(this.returnValue(isWf));
-		emailBody.setKeyword(this.returnValue(keyword));
-		emailBody.setSecretLevel(Integer.valueOf(secretLevel));
-		emailBody.setAuditMan(this.returnValue(auditMan));
-		emailBody.setAuditRemark(this.returnValue(auditRemark));
-		emailBody.setCopyToWebmail(this.returnValue(copyToWebmail));
-		emailBody.setSecretToWebmail(this.returnValue(secretToWebmail));
-		emailBody.setPraise(this.returnValue(praise));
+	String insertEmailBody(
+			@RequestParam(value = "fromId") String fromId,
+			@RequestParam(value = "toId2", required = false) String toId2,
+			@RequestParam(value = "copyToId", required = false) String copyToId,
+			@RequestParam(value = "subject", required = false) String subject,
+			@RequestParam(value = "content", required = false) String content,
+			@RequestParam(value = "attachmentName", required = false) String attachmentName,
+			@RequestParam("sendTime") String sendTime,
+			@RequestParam(value = "sendFlag") String sendFlag,
+			@RequestParam(value = "secretToId", required = false) String secretToId,
+			@RequestParam(value = "attachmentId", required = false) String attachmentId,
+			@RequestParam(value = "smsRemind", required = false) String smsRemind,
+			@RequestParam(value = "important", required = false) String important,
+			@RequestParam(value = "size", required = false) String size,
+			@RequestParam(value = "fromWebmailId", required = false) String fromWebmailId,
+			@RequestParam(value = "fromWebmail", required = false) String fromWebmail,
+			@RequestParam(value = "toWebmail", required = false) String toWebmail,
+			@RequestParam(value = "compressContent", required = false) String compressContent,
+			@RequestParam(value = "webmailContent", required = false) String webmailContent,
+			@RequestParam(value = "webmailFlag", required = false) String webmailFlag,
+			@RequestParam(value = "recvFromName", required = false) String recvFromName,
+			@RequestParam(value = "recvFrom", required = false) String recvFrom,
+			@RequestParam(value = "recvToId", required = false) String recvToId,
+			@RequestParam(value = "recvTo", required = false) String recvTo,
+			@RequestParam(value = "isWebmail", required = false) String isWebmail,
+			@RequestParam(value = "isWf", required = false) String isWf,
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "secretLevel", required = false) String secretLevel,
+			@RequestParam(value = "auditMan", required = false) String auditMan,
+			@RequestParam(value = "auditRemark", required = false) String auditRemark,
+			@RequestParam(value = "copyToWebmail", required = false) String copyToWebmail,
+			@RequestParam(value = "secretToWebmail", required = false) String secretToWebmail,
+			@RequestParam(value = "praise", required = false) String praise) {
 		try {
-			emailService.sendEmail(emailBody, new Email());
-			return JSON.toJSONStringWithDateFormat(
-					new ToJson<EmailBody>(0, "ok"), "yyyy-MM-dd HH:mm:ss");
+			if(sendFlag.equals("0")){
+				
+			}else{
+				emailService.sendEmail(this.returnObj(fromId, toId2, copyToId, subject, content, attachmentName, sendTime, sendFlag, secretToId, attachmentId, smsRemind, important, size, fromWebmailId, fromWebmail, toWebmail, compressContent, webmailContent, webmailFlag, recvFromName, recvFrom, recvToId, recvTo, isWebmail, isWf, keyword, secretLevel, auditMan, auditRemark, copyToWebmail, secretToWebmail, praise)
+						, new Email());
+			}
+			return JSON.toJSONStringWithDateFormat(new ToJson<EmailBody>(0,
+					"ok"), "yyyy-MM-dd HH:mm:ss");
 		} catch (Exception e) {
 			loger.debug("sendMail:" + e);
-			return JSON.toJSONStringWithDateFormat(
-					new ToJson<EmailBody>(1, "errorEmail"), "yyyy-MM-dd HH:mm:ss");
+			return JSON.toJSONStringWithDateFormat(new ToJson<EmailBody>(1,
+					"errorEmail"), "yyyy-MM-dd HH:mm:ss");
 		}
 	}
 
 	/**
 	 * 
-	 * @param request
-	 *            inbox 收件箱 drafts 草稿箱 outbox 发件箱 recycle 废纸篓 noRead 未读
-	 * @return 字符串
-	 * @throws Exception
+	 * @Title: queryEmail
+	 * @Description: 邮件查询
+	 * @author(作者):      zy
+	 * @param: @param  request inbox 收件箱 drafts 草稿箱 outbox 发件箱 recycle 废纸篓 noRead 未读
+	 * @param: @return 字符串
+	 * @param: @throws Exception   
+	 * @return: String   
+	 * @throws
 	 */
 	@RequestMapping(value = "/showEmail", method = RequestMethod.GET, produces = { "application/json;charset=UTF-8" })
 	public @ResponseBody
@@ -156,10 +187,15 @@ public class EmailUtilController {
 	}
 
 	/**
-	 * 根据ID查询内容
-	 * @param request
-	 * @return
-	 * @throws Exception
+	 * 
+	 * @Title: queryByID
+	 * @Description: 根据ID查询内容
+	 * @author(作者):      zy
+	 * @param: @param request Http请求
+	 * @param: @return
+	 * @param: @throws Exception   
+	 * @return: String   
+	 * @throws
 	 */
 	@RequestMapping(value = "/queryByID", method = RequestMethod.GET, produces = { "application/json;charset=UTF-8" })
 	public @ResponseBody
@@ -186,12 +222,17 @@ public class EmailUtilController {
 	}
 
 	/**
-	 * 未读更新为已读
-	 * @param request
-	 * @return
-	 * @throws Exception
+	 * 
+	 * @Title: isRead
+	 * @Description: 未读更新为已读
+	 * @author(作者):      zy
+	 * @param: @param request Http请求
+	 * @param: @return
+	 * @param: @throws Exception   
+	 * @return: String   
+	 * @throws
 	 */
-	@RequestMapping(value = "/isRead", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
+	@RequestMapping(value = "/isRead", method = RequestMethod.GET, produces = { "application/json;charset=UTF-8" })
 	public @ResponseBody
 	String isRead(HttpServletRequest request) throws Exception {
 		String userId = ServletRequestUtils.getStringParameter(request,
@@ -214,28 +255,20 @@ public class EmailUtilController {
 		}
 		return returnRes;
 	}
-	
-	
-	
-	
+
 	/**
-	 * 多条件查询邮件
 	 * 
-	 * @throws Exception
-	 * 
+	 * @Title: querylistEmailBody
+	 * @Description: 多条件查询邮件
+	 * @author(作者):      zy
+	 * @param: @return
+	 * @param: @throws Exception   
+	 * @return: String   
+	 * @throws
 	 */
 	@RequestMapping(value = "/querylistEmailBody", produces = { "application/json;charset=UTF-8" })
 	public @ResponseBody
 	String querylistEmailBody() throws Exception {
-		// SimpleDateFormat format = new
-		// SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		// Date date1 = format.parse("2010-01-06 11:45:55");
-		// Date date2=format.parse("2011-01-06 11:45:55");
-		// Integer starttime = new Integer((int) (date1.getTime()));
-		// Integer endtime=new Integer((int)(date2.getTime()));
-		/**
-		 * 将条件参数存入map集合
-		 */
 		Map<String, Object> maps = new HashMap<String, Object>();
 		// maps.put("startTime",starttime);
 		// maps.put("endTime", endtime);
@@ -269,10 +302,14 @@ public class EmailUtilController {
 	}
 
 	/**
-	 * 为null时转换为""
 	 * 
-	 * @param value
-	 * @return
+	 * @Title: returnValue
+	 * @Description: 为null时转换为""
+	 * @author(作者):      zy
+	 * @param: @param value  需转换字符串
+	 * @param: @return   
+	 * @return: String   
+	 * @throws
 	 */
 	public static String returnValue(String value) {
 		if (value != null) {
@@ -283,19 +320,97 @@ public class EmailUtilController {
 	}
 
 	/**
-	 * 收件箱
 	 * 
-	 * @return
+	 * @Title: returnObj
+	 * @Description: 邮件获取参数封装方法
+	 * @author(作者): zy
+	 * @param: @param fromId 发件人USER_ID,如(lisi,zhangsan,)
+	 * @param: @param toId2 收件人 USER_ID串，每个ID后带一个逗号，如：(lisi,zhangsan,)
+	 * @param: @param copyToId 抄送人USER_ID串,如(lisi,zhangsan,)
+	 * @param: @param subject 邮件主题
+	 * @param: @param content 邮件内容
+	 * @param: @param sendTime 发送时间,如：2017-04-04 10:20:35
+	 * @param: @param attachmentName 附件文件名串
+	 * @param: @param secretToId 密送人USER_ID串,如(lisi,zhangsan,)
+	 * @param: @param attachmentId 附件ID串
+	 * @param: @param sendFlag 是否已发送(0-未发送,1-已发送)
+	 * @param: @param smsRemind 是否使用短信提醒(0-不提醒,1-提醒)
+	 * @param: @param important 重要程度(空-一般邮件,1-重要,2-非常重要)
+	 * @param: @param size 邮件大小
+	 * @param: @param fromWebmailId 从自己的哪个外部邮箱ID对应emailbox中id
+	 * @param: @param fromWebmail 从自己的哪个外部邮箱向外发送
+	 * @param: @param toWebmail 外部收件人邮箱串
+	 * @param: @param compressContent 压缩后的邮件内容
+	 * @param: @param webmailContent 外部邮件内容
+	 * @param: @param webmailFlag 外部邮件标记(0-未发送,1-正在准备发送,2-发送成功,3-发送失败)
+	 * @param: @param recvFromName 接收外部邮箱名称
+	 * @param: @param recvFrom 接收外部邮箱ID
+	 * @param: @param recvToId 发送外部邮件ID
+	 * @param: @param recvTo 发送外部邮箱名称
+	 * @param: @param isWebmail 是否为外部邮件(0-内部邮件,1-外部邮件)
+	 * @param: @param isWf 是否同时外发(0-不外发,1-勾选向此人发送外部邮件)
+	 * @param: @param keyword 内容关键词
+	 * @param: @param secretLevel 邮件密级等级
+	 * @param: @param auditMan 审核人USER_ID
+	 * @param: @param auditRemark 审核不通过备注
+	 * @param: @param copyToWebmail 抄送外部邮箱串
+	 * @param: @param secretToWebmail 抄送外部邮箱串
+	 * @param: @param praise 点赞人user_id串
+	 * @return: EmailBody   
+	 * @throws
 	 */
-	@RequestMapping("/inbox")
-	// 登录窗口
-	public String logins() {
-		return "app/email/inbox";
+	@SuppressWarnings("static-access")
+	public  EmailBody returnObj(String fromId, String toId2, String copyToId,
+			 String subject,String content,String attachmentName,
+			 String sendTime,String sendFlag,String secretToId,
+			 String attachmentId, String smsRemind,String important,
+			 String size,String fromWebmailId,String fromWebmail, String toWebmail,
+			 String compressContent,String webmailContent,String webmailFlag,
+			 String recvFromName,String recvFrom, String recvToId,
+			 String recvTo,String isWebmail, String isWf, String keyword,
+			 String secretLevel, String auditMan, String auditRemark,
+			 String copyToWebmail,String secretToWebmail,String praise){
+		EmailBody emailBody = new EmailBody();
+		emailBody.setFromId(this.returnValue(fromId));
+		emailBody.setToId2(toId2);
+		emailBody.setCopyToId(this.returnValue(copyToId));
+		emailBody.setSecretToId(this.returnValue(secretToId));
+		emailBody.setSubject(this.returnValue(subject));
+		emailBody.setContent(this.returnValue(content));
+		emailBody.setSendTime(DateFormat.getTime(DateFormat.getStrDate(new Date())));
+		emailBody.setAttachmentId(this.returnValue(attachmentId));
+		emailBody.setAttachmentName(this.returnValue(attachmentName));
+		emailBody.setSendFlag(this.returnValue(sendFlag));
+		emailBody.setSmsRemind("0");
+		emailBody.setImportant(this.returnValue(important));
+		emailBody.setSize(0L);
+		emailBody.setFromWebmailId(0);
+		emailBody.setFromWebmail(this.returnValue(fromWebmail));
+		emailBody.setToWebmail(this.returnValue(toWebmail));
+		emailBody.setCompressContent(new byte[0]);
+		emailBody.setWebmailContent(new byte[0]);
+		emailBody.setWebmailFlag("0");
+		emailBody.setRecvFromName(this.returnValue(recvFromName));
+		emailBody.setRecvFrom(this.returnValue(recvFrom));
+		emailBody.setRecvToId(0);
+		emailBody.setRecvTo(this.returnValue(recvTo));
+		emailBody.setIsWebmail("0");
+		emailBody.setIsWf("0");
+		emailBody.setKeyword(this.returnValue(keyword));
+		if (secretLevel == null) {
+			emailBody.setSecretLevel(0);
+		} else {
+			emailBody.setSecretLevel(Integer.valueOf(secretLevel));
+		}
+		emailBody.setAuditMan(this.returnValue(auditMan));
+		emailBody.setAuditRemark(this.returnValue(auditRemark));
+		emailBody.setCopyToWebmail(this.returnValue(copyToWebmail));
+		emailBody.setSecretToWebmail(this.returnValue(secretToWebmail));
+		emailBody.setPraise(this.returnValue(praise));
+		return emailBody;
 	}
+	
 
-	/**
-	 * 根据ID删除一条邮件`
-	 */
 	/*
 	 * @RequestMapping(value = "/deleteEmailBody", produces = {
 	 * "application/json;charset=UTF-8" }) public @ResponseBody void
