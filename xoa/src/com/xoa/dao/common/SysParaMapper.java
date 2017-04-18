210e007a94b0be32b5bcf0a1adc602d9d177a778
wangyueqi <843209972@qq.com>
2017-04-18 09:12:17 +0800

    提交

---
+++ xoa/src/com/xoa/dao/common/SysParaMapper.java
@@ -0,0 +1,24 @@
+package com.xoa.dao.common;
+
+import com.xoa.model.common.SysPara;
+/**
+ * 
+ * @ClassName (类名):  SysParaMapper
+ * @Description(简述): 移动端对应
+ * @author(作者):      wyq
+ * @date(日期):        2017-4-17 下午5:52:49
+ *
+ */
+public interface SysParaMapper {
+	
+	/**
+	 * 
+	 * @Title: getSysPara
+	 * @Description: 查询移动端对应的菜单ID串
+	 * @author(作者):      wyq
+	 * @param: @return   
+	 * @return: String   
+	 * @throws
+	 */
+    public String getSysPara();
+}
