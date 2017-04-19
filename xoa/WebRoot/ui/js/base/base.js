b2a4bfa06a8fa47584a15ad9ac1cd3395a2fa1fe
zhuzy <22@ee.com>
2017-04-19 17:54:10 +0800

    2

--- xoa/WebRoot/ui/js/base/base.js
+++ xoa/WebRoot/ui/js/base/base.js
@@ -5,7 +5,7 @@
 			var left = left || '100';
 			var width = width || '800';
 			var height = height || '800';
-			window.open(url,"",top,left,width,height);
+			window.open(url,top,left,width,height);
 	    }
 	});
 	$.extend({
