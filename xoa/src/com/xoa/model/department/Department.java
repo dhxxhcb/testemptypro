fc118a2b522cebccbc6760421ff51a5debff716e
zlf <3476293819@qq.com>
2017-04-25 17:20:21 +0800

    修改接口

--- xoa/src/com/xoa/model/department/Department.java
+++ xoa/src/com/xoa/model/department/Department.java
@@ -93,9 +93,17 @@ public class Department {
     
     private String userPrivName;
     
+    private String type;
     
-    
-    public String getAvatar() {
+    public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAvatar() {
 		return avatar;
 	}
 
