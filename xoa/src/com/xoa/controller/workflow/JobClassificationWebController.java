98aa06c9e3a1f678d63a0d5bc6ca39fdc8df1fb0
handtpfl <2211869388@qq.com>
2017-05-05 11:09:37 +0800

    更改路径

--- xoa/src/com/xoa/controller/workflow/JobClassificationWebController.java
+++ xoa/src/com/xoa/controller/workflow/JobClassificationWebController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by 王曰岐 on 2017/5/5.
  */
 @Controller
-@RequestMapping("/workflow/page")
+@RequestMapping("/workflow/flowclassify")
 public class JobClassificationWebController {
 
-    @RequestMapping("/jobClass")
+    @RequestMapping("/index")
     public String showPage(Model model){
         return "app/workflow/flowclassify/index";
     }
