6b9d4b91eee226a3edf2412bd1f00f7885c9938f
zlf <3476293819@qq.com>
2017-05-05 16:56:38 +0800

    ca

--- xoa/src/com/xoa/controller/users/UsersController.java
+++ xoa/src/com/xoa/controller/users/UsersController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.xoa.model.users.Users;
 import com.xoa.service.users.UsersService;
 import com.xoa.util.ToJson;
--- xoa/src/com/xoa/service/schedule/impl/CalenderServiceImpl.java
+++ xoa/src/com/xoa/service/schedule/impl/CalenderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.xoa.service.schedule.impl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,17 +37,63 @@ public class CalenderServiceImpl implements CalenderService {
 	}
 	/**
 	 * 创建作者:   张龙飞
-	 * 创建日期:   2017年5月4日 下午5:24:26
+	 * 创建日期:   2017年5月4日 下午5:24:26+-
 	 * 方法介绍:   根据userId 查询日程安排
-	 * 参数说明:   @param userId 用户userId
+	 * 参数说明:   @param Map<String, Object> maps 用户userId和当天时间戳
 	 * 参数说明:   @return
 	 * @return     List<Calendar>  返回日程安排
+	 * @throws Exception 
 	 */
 	@Override
-	public List<Calendar> getscheduleBycId(String userId) {
-		List<Calendar> list =calendarMapper.getscheduleBycId(userId);
+	public List<Calendar> getscheduleBycId(String userId,int calTime) throws Exception {
+		Map<String, Object> maps =new HashMap<String, Object>();
+		maps.put("userId", userId);
+		maps.put("calTime", calTime);
+		List<Calendar> list =calendarMapper.getscheduleBycId(maps);
+		Long day=Long.valueOf(calTime);
+		SimpleDateFormat format =  new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String d = format.format(day);  
+		Date date=format.parse(d);  
+		date.setMonth(date.getMonth()+1);
+		//System.out.println(date.toString());
+		//下个月的时间戳
+		Long time=date.getTime();
+		
+		
+		/*java.util.Calendar cal= java.util.Calendar.getInstance();
+		//设置年
+		cal.set(cal.YEAR,date.getYear());
+		//设置月
+		cal.set(cal.MONTH,date.getMonth()+1);	
+		//设置日
+		cal.set(cal.DAY_OF_MONTH,date.getDay());
+	    String MonthDay = format.format(cal.getTime());*/
+		for(int i=0;i<list.size();i++){
+			
+			Calendar calendar=list.get(i);
+			int cT=calendar.getCalTime();
+			int eT=calendar.getEndTime();
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
 		return list;
 	}
+	/**
+     * 创建作者:   张龙飞
+     * 创建日期:   2017年5月4日 下午5:46:54
+     * 方法介绍:   新增日程安排
+     * 参数说明:   @param record 日程安排
+     * 参数说明:   @return
+     * @return     int 插入条数
+     */
 	@Override
 	public int insertSelective(Calendar record) {
 		int count=calendarMapper.insertSelective(record);
