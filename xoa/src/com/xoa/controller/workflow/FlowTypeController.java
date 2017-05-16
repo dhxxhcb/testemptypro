3f92e6b36befb0155eadaf1df023e87500bed5d5
zhuzy <22@ee.com>
2017-05-16 11:41:18 +0800

    添加  流程设计器

--- xoa/src/com/xoa/controller/workflow/FlowTypeController.java
+++ xoa/src/com/xoa/controller/workflow/FlowTypeController.java
@@ -6,6 +6,7 @@ import com.xoa.util.ToJson;
 import com.xoa.util.dataSource.ContextHolder;
 import org.apache.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,7 +39,19 @@ public class FlowTypeController {
                 "loginDateSouse"));
         return flowTypeService.saveFlow(flowTypeModel);
     }
-
+    /**
+     * 
+     * 创建作者:  朱振宇
+     * 创建日期:  2017-5-16 上午11:27:16
+     * 方法介绍:  
+     * 参数说明:  @param model
+     * 参数说明:  @return
+     * @return    String
+     */
+    @RequestMapping("/flowtype/flowdesigner")
+    public String flowdesigner(Model model){
+        return "app/workflow/flowtype/flowdesigner";
+    }
 
     @RequestMapping("/flowNews")
     public String flow(HttpServletRequest request){
