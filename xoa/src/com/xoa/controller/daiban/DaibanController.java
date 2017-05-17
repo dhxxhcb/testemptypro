fda11bb50994aa2103c0fca84447b9ce7b771b29
zlf <3476293819@qq.com>
2017-05-17 15:25:02 +0800

    xg

--- xoa/src/com/xoa/controller/daiban/DaibanController.java
+++ xoa/src/com/xoa/controller/daiban/DaibanController.java
@@ -9,19 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xoa.model.daiban.Daiban;
-import com.xoa.model.department.Department;
-import com.xoa.model.users.Users;
 import com.xoa.service.daiban.DaibanService;
 import com.xoa.util.ToJson;
-import com.xoa.util.common.L;
-import com.xoa.util.common.StringUtils;
-import com.xoa.util.common.session.SessionUtils;
 import com.xoa.util.dataSource.ContextHolder;
 
 @Controller
--- xoa/src/com/xoa/model/daiban/Daiban.java
+++ xoa/src/com/xoa/model/daiban/Daiban.java
@@ -7,6 +7,48 @@ import com.xoa.model.notify.Notify;
 
 public class Daiban {
 	
+	private String emainContent;
+	
+	private String notifyContent;
+
+	public String getEmainContent() {
+		return emainContent;
+	}
+
+	public void setEmainContent(String emainContent) {
+		this.emainContent = emainContent;
+	}
+
+	public String getNotifyContent() {
+		return notifyContent;
+	}
+
+	public void setNotifyContent(String notifyContent) {
+		this.notifyContent = notifyContent;
+	}
+	
+	private String emailUrl;
+	
+	private String notifyUrl;
+	
+	
+
+	public String getEmailUrl() {
+		return emailUrl;
+	}
+
+	public void setEmailUrl(String emailUrl) {
+		this.emailUrl = emailUrl;
+	}
+
+	public String getNotifyUrl() {
+		return notifyUrl;
+	}
+
+	public void setNotifyUrl(String notifyUrl) {
+		this.notifyUrl = notifyUrl;
+	}
+
 	private List<EmailBodyModel> email;
 	
 	private List<Notify> notify;
