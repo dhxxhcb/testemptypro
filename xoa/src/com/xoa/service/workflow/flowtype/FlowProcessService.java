c2e5a57bc8a2a0d6a9a95adcbd2a08a469b591d1
zlf <3476293819@qq.com>
2017-05-22 10:20:52 +0800

    查询流程

--- xoa/src/com/xoa/service/workflow/flowtype/FlowProcessService.java
+++ xoa/src/com/xoa/service/workflow/flowtype/FlowProcessService.java
@@ -13,11 +13,58 @@ import com.xoa.util.ToJson;
  *
  */
 public interface FlowProcessService {
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:42:23
+	 * 方法介绍:   根据id查找流程
+	 * 参数说明:   @param id 编号
+	 * 参数说明:   @return
+	 * @return     FlowProcess 流程
+	 */
 	public FlowProcess find(int id);
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:43:03
+	 * 方法介绍:   修改流程
+	 * 参数说明:   @param record 流程信息
+	 * 参数说明:   @return
+	 * @return     ToJson<FlowProcess> 返回流程信息
+	 */
 	public ToJson<FlowProcess> updateByPrimaryKeySelective(FlowProcess record);
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:43:58
+	 * 方法介绍:   根据流程id查找流程
+	 * 参数说明:   @param flowId  流程id
+	 * 参数说明:   @return
+	 * @return     List<FlowProcess> 返回流程信息
+	 */
 	public List<FlowProcess> findFlowId(int flowId);
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:44:40
+	 * 方法介绍:   删除流程
+	 * 参数说明:   @param id 
+	 * @return     void
+	 */
 	public void delete(int id);
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:45:01
+	 * 方法介绍:   根据流程id查找流程
+	 * 参数说明:   @param flowId 流程id
+	 * 参数说明:   @return
+	 * @return     FlowProcess 流程信息
+	 */
 	public FlowProcess flowView(int flowId);
+	/**
+	 * 创建作者:   张龙飞
+	 * 创建日期:   2017年5月22日 上午9:45:24
+	 * 方法介绍:   新增流程
+	 * 参数说明:   @param record
+	 * 参数说明:   @return
+	 * @return     int
+	 */
 	public int insertSelective(FlowProcess record);
 	
 	
