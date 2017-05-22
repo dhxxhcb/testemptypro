c8b9b030b79664b48853a228b5deaa095cdb738d
wangyueqi <843209972@qq.com>
2017-05-22 15:16:08 +0800

    15：15

--- xoa/src/com/xoa/controller/common/CommonController.java
+++ xoa/src/com/xoa/controller/common/CommonController.java
@@ -55,5 +55,23 @@ public class CommonController {
 				"loginDateSouse"));
 		return "app/common/addPageDemo";
 	}
+	@RequestMapping("/divisionalManagement")
+	public String divisionalManagement(HttpServletRequest request) {
+		ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+				"loginDateSouse"));
+		return "app/sys/divisionalManagement";
+	}
+	@RequestMapping("/roleManagement")
+	public String roleManagement(HttpServletRequest request) {
+		ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+				"loginDateSouse"));
+		return "app/sys/roleManagement";
+	}
+	@RequestMapping("/userManagement")
+	public String userManagement(HttpServletRequest request) {
+		ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+				"loginDateSouse"));
+		return "app/sys/userManagement";
+	}
 	
 }
