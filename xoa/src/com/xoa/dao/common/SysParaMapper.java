ac95105aac4463107ea5b38e7c4ad728e3a33176
hanchengbing <hhccbb123@163.com>
2017-05-23 17:38:45 +0800

    修改功能

--- xoa/src/com/xoa/dao/common/SysParaMapper.java
+++ xoa/src/com/xoa/dao/common/SysParaMapper.java
@@ -1,52 +1,52 @@
 package com.xoa.dao.common;
 
-import java.util.List;
-
 import com.xoa.model.common.SysPara;
 
-    /**
-     * 
-     * 创建作者:   王曰岐
-     * 创建日期:   2017-4-18 下午6:18:03
-     * 类介绍    :   APP对应DAO
-     * 构造参数:   无
-     *
-     */
+import java.util.List;
+
+/**
+ * 创建作者:   王曰岐
+ * 创建日期:   2017-4-18 下午6:18:03
+ * 类介绍    :   APP对应DAO
+ * 构造参数:   无
+ */
 public interface SysParaMapper {
-	
-	/**
-	 * 
-	 * 创建作者:   王曰岐
-	 * 创建日期:   2017-4-18 下午6:18:48
-	 * 方法介绍:   得到APP端对应的菜单
-	 * 参数说明:   @return
-	 * @return     String 返回APP对应的菜单串
-	 */
-    public String getSysPara();
+
     /**
-     * 
+     * 创建作者:   王曰岐
+     * 创建日期:   2017-4-18 下午6:18:48
+     * 方法介绍:   得到APP端对应的菜单
+     * 参数说明:   @return
+     *
+     * @return String 返回APP对应的菜单串
+     */
+    public String getSysPara();
+
+    /**
+     * @throws
      * @Title: getIeTitle
      * @Description: 状态栏查询方法
      * @author(作者): 张丽军
-     * @param: @return   
-     * @return: List<com.xoa.model.common.SysPara>   
-     * @throws
+     * @param: @return
+     * @return: List<com.xoa.model.common.SysPara>
      */
-	List<SysPara> getIeTitle();
-	
-	public List<SysPara> getIeTitle1();
+    List<SysPara> getIeTitle();
+
+    public List<SysPara> getIeTitle1();
+
     /**
-     * 
+     * @throws
      * @Title: updateSysPara
      * @Description: 状态栏修改方法
      * @author(作者): 张丽军
-     * @param: @param sysPara   
-     * @return: void   
-     * @throws
+     * @param: @param sysPara
+     * @return: void
      */
-	void updateSysPara(SysPara sysPara);
-	
-	public void updateSysPara1(SysPara sysPara);
-	
-	
+    void updateSysPara(SysPara sysPara);
+
+    public void updateSysPara1(SysPara sysPara);
+
+
+    List<SysPara> getTheSysParam(String paraName);
+
 }
