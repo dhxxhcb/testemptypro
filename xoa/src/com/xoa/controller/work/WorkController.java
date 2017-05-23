470e93d7fcc371f7d764eddea7511a611559d530
zlf <3476293819@qq.com>
2017-05-23 17:31:30 +0800

    xg

--- xoa/src/com/xoa/controller/work/WorkController.java
+++ xoa/src/com/xoa/controller/work/WorkController.java
@@ -1,11 +1,18 @@
 package com.xoa.controller.work;
 
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.xoa.model.workflow.FlowFast;
+import com.xoa.util.ToJson;
 import com.xoa.util.dataSource.ContextHolder;
 
 /**
@@ -20,7 +27,7 @@ import com.xoa.util.dataSource.ContextHolder;
 @RequestMapping("/workflow/work")
 public class WorkController {
 	
-	@RequestMapping("/addwork")
+		@RequestMapping("index")
 	    public String work(HttpServletRequest request) {
 	        ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
 	                "loginDateSouse"));
@@ -39,5 +46,29 @@ public class WorkController {
 				"loginDateSouse"));
 		return "app/workflow/work/workform";
 	}
-
+	
+	/*public ToJson<FlowFast> fastAdd(HttpServletRequest request,
+			int flowId){
+		ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+				"loginDateSouse"));
+	}*/
+	
+	@RequestMapping("addFastwork")
+	public String addwork(String str,HttpServletRequest request) throws JSONException{
+		ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+				"loginDateSouse"));
+	    JSONObject hostObject = new JSONObject();  
+	    Iterator<String> sIterator = hostObject.keys();  
+	    while(sIterator.hasNext()){  
+	        // 获得key  
+	        String key = sIterator.next();  
+	        // 根据key获得value, value也可以是JSONObject,JSONArray,使用对应的参数接收即可  
+	        String value = hostObject.getString(key);  
+	        System.out.println("key: "+key+",value"+value);  
+	    }  
+        
+		
+		
+		return "";
+	}
 }
