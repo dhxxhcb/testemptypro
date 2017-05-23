025120a1a879d3aad3d5c47bf36b531e7f8a77cd
zhangyong <349101912@qq.com>
2017-05-23 17:55:14 +0800

    流程自定义属性 查询

--- xoa/src/com/xoa/service/workflow/flowtype/FlowTypeService.java
+++ xoa/src/com/xoa/service/workflow/flowtype/FlowTypeService.java
@@ -3,6 +3,8 @@ package com.xoa.service.workflow.flowtype;
 import com.xoa.model.workflow.FlowTypeModel;
 import com.xoa.util.ToJson;
 
+import java.util.Map;
+
 /**
  * 创建作者:   张勇
  * 创建日期:   2017/5/8 16:39
@@ -19,6 +21,16 @@ public interface FlowTypeService {
          * @return
          */
         public ToJson<FlowTypeModel> saveFlow(FlowTypeModel flowTypeModel);
+
+        /**
+         * 创建作者:   张勇
+         * 创建日期:   2017/5/23 17:30
+         * 方法介绍:   自定义属性所有查询，根据flowId
+         * 参数说明:
+         * @return
+         */
+        public  ToJson<FlowTypeModel> selectAllFlow(Map<String, Object> maps);
+
 
 
         public ToJson<FlowTypeModel> quertBySortId(Integer flowId);
