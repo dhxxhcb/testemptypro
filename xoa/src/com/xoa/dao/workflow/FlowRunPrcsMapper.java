cd7866e662010fe133a6b624f6970d9732b8f0d3
zhangyong <349101912@qq.com>
2017-05-24 20:32:17 +0800

    待办和完结 两个接口业务层

--- xoa/src/com/xoa/dao/workflow/FlowRunPrcsMapper.java
+++ xoa/src/com/xoa/dao/workflow/FlowRunPrcsMapper.java
@@ -1,8 +1,22 @@
 package com.xoa.dao.workflow;
 
+import com.xoa.dao.base.BaseMapper;
 import com.xoa.model.workflow.FlowRunPrcs;
 
-public interface FlowRunPrcsMapper {
+import java.util.List;
+import java.util.Map;
+
+public interface FlowRunPrcsMapper extends BaseMapper<FlowRunPrcs>{
+
+    /**
+     * 创建作者:   张勇
+     * 创建日期:   2017/5/24 20:16
+     * 方法介绍:   查询完结工作
+     * 参数说明:
+     * @return
+     */
+    public List<FlowRunPrcs>  selectEnd(Map<String,Object> maps);
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(FlowRunPrcs record);
--- xoa/src/com/xoa/service/workflow/flowtype/impl/FlowRunPrcsServiceImpl.java
+++ xoa/src/com/xoa/service/workflow/flowtype/impl/FlowRunPrcsServiceImpl.java
@@ -2,11 +2,15 @@ package com.xoa.service.workflow.flowtype.impl;
 
 import javax.annotation.Resource;
 
+import com.xoa.util.ToJson;
 import org.springframework.stereotype.Service;
 
 import com.xoa.dao.workflow.FlowRunPrcsMapper;
 import com.xoa.model.workflow.FlowRunPrcs;
 import com.xoa.service.workflow.flowtype.FlowRunPrcsService;
+
+import java.util.Map;
+
 @Service
 public class FlowRunPrcsServiceImpl implements FlowRunPrcsService {
 	
@@ -16,6 +20,34 @@ public class FlowRunPrcsServiceImpl implements FlowRunPrcsService {
 	@Override
 	public void save(FlowRunPrcs flowRunPrcs) {
 		flowRunPrcsMapper.insert(flowRunPrcs);
+	}
+
+	/**
+	 * 创建作者:   张勇
+	 * 创建日期:   2017/5/24 20:29
+	 * 方法介绍:   查询代办工作
+	 * 参数说明:
+	 * @return
+	 */
+	@Override
+	public ToJson<FlowRunPrcs> selectObject(Map<String, Object> maps) {
+		ToJson<FlowRunPrcs> toJson = new ToJson<FlowRunPrcs>();
+
+
+
+		return null;
+	}
+
+	/**
+	 * 创建作者:   张勇
+	 * 创建日期:   2017/5/24 20:30
+	 * 方法介绍:   查询办结工作
+	 * 参数说明:
+	 * @return
+	 */
+	@Override
+	public ToJson<FlowRunPrcs> selectEnd(Map<String, Object> maps) {
+		return null;
 	}
 
 }
