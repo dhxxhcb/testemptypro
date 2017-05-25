246cef7fa343952c8f95886549d02030105c0ff4
wangyueqi <843209972@qq.com>
2017-05-25 15:03:00 +0800

    15:02

--- xoa/src/com/xoa/controller/users/UserFunctionController.java
+++ xoa/src/com/xoa/controller/users/UserFunctionController.java
@@ -2,12 +2,20 @@ package com.xoa.controller.users;
 
 import javax.annotation.Resource;
 
+import com.alibaba.fastjson.JSON;
+import com.xoa.model.menu.SysFunction;
+import com.xoa.util.ToJson;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.xoa.controller.menu.MenuController;
 import com.xoa.service.users.UserFunctionService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -18,13 +26,21 @@ public class UserFunctionController {
 	
 	@Resource
 	private UserFunctionService  userFunctionService;
+
+
+	/*	@RequestMapping("/showGetMenu")
+	@ResponseBody
+	public ToJson<SysFunction>  getMenu(){
+		ToJson<SysFunction> sysList=userFunctionService.getMenu(1);
+   return  sysList;
+	}*/
 	
 	/*@RequestMapping("/showUser2")
 	@ResponseBody
 	public String show(){
 		ToJson<SysFunction> sysList=userFunctionService.getMenu(63);
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("showmenus", JSON.toJSONStringWithDateFormat(sysList, "yyyy-MM-dd HH:mm:ss"));
+		//map.put("showmenus", JSON.toJSONStringWithDateFormat(sysList, "yyyy-MM-dd HH:mm:ss"));
 		return JSON.toJSONStringWithDateFormat(sysList, "yyyy-MM-dd HH:mm:ss");
 		
 	}*/
