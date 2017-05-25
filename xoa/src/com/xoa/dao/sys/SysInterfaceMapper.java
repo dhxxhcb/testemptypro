f9f7634988ab5e7003ca27c2a53371f7be176c99
hanchengbing <hhccbb123@163.com>
2017-05-25 14:42:39 +0800

    状态栏设置

---
+++ xoa/src/com/xoa/dao/sys/SysInterfaceMapper.java
@@ -0,0 +1,18 @@
+package com.xoa.dao.sys;
+
+import com.xoa.model.sys.InterfaceModel;
+
+import java.util.List;
+
+/**
+ *
+ */
+
+public interface SysInterfaceMapper {
+
+
+    List<InterfaceModel> getStatusText();
+
+    void editStatusText(String statusText);
+}
+
