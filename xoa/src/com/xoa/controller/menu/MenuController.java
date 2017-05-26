d13d5a446b403a830b4ff4382e776a98c39f6572
hanchengbing <hhccbb123@163.com>
2017-05-26 17:05:40 +0800

    菜单修改

--- xoa/src/com/xoa/controller/menu/MenuController.java
+++ xoa/src/com/xoa/controller/menu/MenuController.java
@@ -240,8 +240,8 @@ public class MenuController {
      **/
     @RequestMapping(value = "/deleteSysMenu", produces = {"application/json;charset=UTF-8"})
     public String deleteSysMenu(String id, HttpServletRequest request) {
-      /*  ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
-                "loginDateSouse"));*/
+        ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+                "loginDateSouse"));
         //id不为空，去除空格，id长度大于等于2
         if (id != null && id.trim().length() >= 2) {
             try {
@@ -265,8 +265,8 @@ public class MenuController {
     @ResponseBody
     @RequestMapping(value = "/addFunction", produces = {"application/json;charset=UTF-8"})
     public ToJson<SysFunction> addFunctionMenu(SysFunction sysFunction, HttpServletRequest request, String parentId) {
-       /* ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
-                "loginDateSouse"));*/
+        ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+                "loginDateSouse"));
         //localhost:8081/addFunction?fid=343&id=76&name=myname&parentId=1090&url=myurl
         //ext是 国际版多语言菜单名称，先用“”，留好接口
         sysFunction.setExt("");
@@ -330,12 +330,12 @@ public class MenuController {
     }
 
     /**
-     * @创建作者: 韩成冰
-     * @创建日期: 2017/5/26 16:53
-     * @函数介绍: 查询2/3级菜单
-     * @参数说明: @param id 菜单id
-     * @return: Json
-     **/
+    *@创建作者:  韩成冰
+    *@创建日期:  2017/5/26 16:53
+    *@函数介绍: 查询2/3级菜单
+    *@参数说明:  @param id 菜单id
+    *@return:   Json
+    **/
     @ResponseBody
     @RequestMapping(value = "/findChildMenu", produces = {"application/json;charset=UTF-8"})
     public ToJson<SysFunction> findChildMenu(String id, HttpServletRequest request) {
