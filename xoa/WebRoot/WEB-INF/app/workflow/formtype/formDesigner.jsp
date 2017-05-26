b7e6e5d4a0303bfe06923c0368ebae9cc8fb80d9
wangyueqi <843209972@qq.com>
2017-05-26 20:22:15 +0800

    20.22

--- xoa/WebRoot/WEB-INF/app/workflow/formtype/formDesigner.jsp
+++ xoa/WebRoot/WEB-INF/app/workflow/formtype/formDesigner.jsp
@@ -58,7 +58,9 @@
                     <img src="../img/icon_checkbox_03.png" alt="">复选框</a></li>
                 <li><a href="javascript:void(0);" type="macros" class="btn btn-link">
                     <img src="../img/icon_hongcontrol_03.png" alt="">宏控件</a></li>
-                <li><a href="javascript:void(0);" type="calendar" class="btn btn-link">
+                <li><a href="javascript:void(0);" type="listing" class="btn btn-link">
+                    <img src="../img/icon_hongcontrol_03.png" alt="">列表控件</a></li>
+               <li><a href="javascript:void(0);" type="calendar" class="btn btn-link">
                     <img src="../img/icon_calendar_03.png" alt="">日历控件</a></li>
                 <li><a href="javascript:void(0);">
                     <img src="../img/icon_calculator_03.png" alt="">计算控件</a></li>
--- xoa/WebRoot/ui/lib/ueditor/formdesign/formdesign.v4.js
+++ xoa/WebRoot/ui/lib/ueditor/formdesign/formdesign.v4.js
@@ -9,6 +9,81 @@
 */
 UE.formDesignUrl = 'formdesign';
 /**
+ * 列表控件
+ * @command textfield
+ * @method execCommand
+ * @param { String } cmd 命令字符串
+ * @example
+ * ```javascript
+ * editor.execCommand( 'textfield');
+ * ```
+ */
+UE.plugins['listing'] = function () {
+    var me = this,thePlugins = 'listing';
+    me.commands[thePlugins] = {
+        execCommand:function () {
+            var dialog = new UE.ui.Dialog({
+                iframeUrl:this.options.UEDITOR_HOME_URL + UE.formDesignUrl+'/listing.html?dataid='+(pluginId++),
+                name:thePlugins,
+                editor:this,
+                title: '单行输入框',
+                cssRules:"width:600px;height:380px;",
+                buttons:[
+                    {
+                        className:'edui-okbutton',
+                        label:'确定',
+                        onclick:function () {
+                            dialog.close(true);
+                        }
+                    },
+                    {
+                        className:'edui-cancelbutton',
+                        label:'取消',
+                        onclick:function () {
+                            dialog.close(false);
+                        }
+                    }]
+            });
+            dialog.render();
+            dialog.open();
+        }
+    };
+    var popup = new baidu.editor.ui.Popup( {
+        editor:this,
+        content: '',
+        className: 'edui-bubble',
+        _edittext: function () {
+            baidu.editor.plugins[thePlugins].editdom = popup.anchorEl;
+            me.execCommand(thePlugins);
+            this.hide();
+        },
+        _delete:function(){
+            if( window.confirm('确认删除该控件吗？') ) {
+                baidu.editor.dom.domUtils.remove(this.anchorEl,false);
+            }
+            this.hide();
+        }
+    } );
+    popup.render();
+    me.addListener( 'mouseover', function( t, evt ) {
+
+        evt = evt || window.event;
+        var el = evt.target || evt.srcElement;
+        var leipiPlugins = el.getAttribute('data-type');
+        if ( /input/ig.test( el.tagName )  && leipiPlugins==thePlugins) {
+            var html = popup.formatHtml(
+                '<nobr>单行输入框: <span onclick=$$._edittext() class="edui-clickable">编辑</span>&nbsp;&nbsp;<span onclick=$$._delete() class="edui-clickable">删除</span></nobr>' );
+            if ( html ) {
+                popup.getDom( 'content' ).innerHTML = html;
+                popup.anchorEl = el;
+                popup.showAnchor( popup.anchorEl );
+            } else {
+                popup.hide();
+            }
+        }
+    });
+};
+/**
  * 单行输入框
  * @command textfield
  * @method execCommand
