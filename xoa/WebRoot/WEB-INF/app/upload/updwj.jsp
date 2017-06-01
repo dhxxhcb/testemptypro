89fe16b9421c9e366f25906da72fa3943cc93a21
wangyueqi <843209972@qq.com>
2017-06-01 17:55:05 +0800

    17:55

--- xoa/WebRoot/WEB-INF/app/upload/updwj.jsp
+++ xoa/WebRoot/WEB-INF/app/upload/updwj.jsp
@@ -10,45 +10,45 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 <script type="text/javascript">
-    function doUpload() {  
-         var file = new FormData($( "#uploadForm" )[1]);  
+    function doUpload() {
+         var file = new FormData($( "#uploadForm" )[1]);
          console.log($( "#uploadForm" ).serialize());
-         $.ajax({  
-              url: 'http://localhost:8080/xoa/upload'+'?module=email',  
-              type: 'POST',  
+         $.ajax({
+              url: 'http://localhost:8080/xoa/upload'+'?module=email',
+              type: 'POST',
               data: new FormData($('#uploadForm')[1]),
               dataType:"text",
-              success: function (returndata) {  
+              success: function (returndata) {
               	console.log(returndata.innerHTML);
-                 
-              },  
-              error: function (XMLHttpRequest, textStatus, errorThrown) {  
+
+              },
+              error: function (XMLHttpRequest, textStatus, errorThrown) {
                   console.log(XMLHttpRequest.status);
 				console.log(XMLHttpRequest.readyState);
 				console.log(textStatus);
-              }  
-         });  
-    }  
+              }
+         });
+    }
 
-</script> 
+</script>
 </head>
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <body>
-<form action="upload?module=email" method="post" enctype="multipart/form-data">  
-    <input type="file" name="file" /> 
+<form action="upload?module=email" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" />
     <input type="file" name="file" />
     <input type="file" name="file" />
     <input type="submit" value="Submit" />
-</form> 
-   
-    <a href="<%=basePath%>WEB-INF/app/updanwenjian.jsp">批量</a> 
-    
-    <form id= "uploadForm">  
-      <p >指定文件名： <input type="text" name="filename" value= ""/></p >  
-      <p >上传文件： <input type="file" name="file"/></ p>  
-      <input type="button" value="上传" onclick="doUpload()" />  
 </form>
-    
-    
+
+    <a href="<%=basePath%>WEB-INF/app/updanwenjian.jsp">批量</a>
+
+    <form id= "uploadForm">
+      <p >指定文件名： <input type="text" name="filename" value= ""/></p >
+      <p >上传文件： <input type="file" name="file"/></ p>
+      <input type="button" value="上传" onclick="doUpload()" />
+</form>
+
+
 </body>
 </html>
--- xoa/src/com/xoa/controller/netdisk/NetdiskController.java
+++ xoa/src/com/xoa/controller/netdisk/NetdiskController.java
@@ -14,18 +14,22 @@ import com.xoa.util.dataSource.ContextHolder;
 import com.xoa.util.netdisk.CheckAll;
 import com.xoa.util.netdisk.ReadFile;
 import org.apache.http.HttpRequest;
+import org.apache.poi.util.SystemOutLogger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by gsb on 2017/6/1.
@@ -38,7 +42,13 @@ public class NetdiskController {
     @Resource
     private NetdiskService netdiskService;
 
-
+    /**
+     * 查找目录文件
+     * @param request
+     * @param path
+     * @return
+     * @throws Exception
+     */
     @ResponseBody
     @RequestMapping(value="/selectNetdisk",produces = {"application/json;charset=UTF-8"})
     public  ToJson<Catalog> selectNetdisk(HttpServletRequest request,String path) throws Exception {
@@ -77,6 +87,91 @@ public class NetdiskController {
 
         return toJson;
     }
+
+    /**
+     * 新增文件目录
+     * @param request
+     * @param path
+     * @param directoryName
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value="/mkDirectory",produces = {"application/json;charset=UTF-8"})
+    public ToJson<String> mkDirectory(HttpServletRequest request,String path,String directoryName ){
+        ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+                "loginDateSouse"));
+        String mkDirectoryPath =path+"/"+directoryName;
+        System.out.print("sasasa"+mkDirectoryPath);
+        ToJson<String> json=new ToJson<String>();
+        if (ReadFile.mkDirectory(mkDirectoryPath)) {
+            json.setFlag(0);
+            json.setMsg("ok");
+
+        }
+        else{
+            json.setFlag(1);
+            json.setMsg("err");
+
+        }
+
+        return  json;
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/uploadFile",method={RequestMethod.POST},produces = {"application/json;charset=UTF-8"})
+    public ToJson<String>  uploadFile(HttpServletRequest request, HttpServletResponse response,String path) throws IOException {
+        //创建一个通用的多部分解析器
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        ToJson<String>  json=new ToJson<String>();
+    //判断 request 是否有文件上传,即多部分请求
+        if(multipartResolver.isMultipart(request)){
+            //转换成多部分request
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;
+            //取得request中的所有文件名
+            Iterator<String> iter = multiRequest.getFileNames();
+            while(iter.hasNext()){
+                //记录上传过程起始时的时间，用来计算上传时间
+                int pre = (int) System.currentTimeMillis();
+                //取得上传文件
+                MultipartFile file = multiRequest.getFile(iter.next());
+                if(file != null){
+                    //取得当前上传文件的文件名称
+                    String myFileName = file.getOriginalFilename();
+                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在
+                    if(myFileName.trim() !=""){
+                        System.out.println(myFileName);
+                        //重命名上传后的文件名
+                        String fileName =  file.getOriginalFilename();
+                        //定义上传路径
+                        String path1 = path +"/"+ fileName;
+                        File localFile = new File(path1);
+                        file.transferTo(localFile);
+                        json.setFlag(0);
+                        json.setMsg("ok");
+                    }else {
+                        json.setFlag(1);
+                        json.setMsg("err");
+                    }
+                }
+                //记录上传该文件后的时间
+                int finaltime = (int) System.currentTimeMillis();
+                System.out.println(finaltime - pre);
+            }
+
+        }
+
+
+        return  json;
+    }
+
+
+    @RequestMapping("/up1")
+    public String cont(HttpServletRequest request) {
+        ContextHolder.setConsumerType("xoa" + (String) request.getSession().getAttribute(
+                "loginDateSouse"));
+        return "app/upload/updwj1";
+    }
+
 
 
 
