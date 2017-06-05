d10b2a765f24d280495b5f5e8c2fe880521536a9
张勇 <349101912@qq.com>
2017-06-05 17:34:54 +0800

    邮箱的小bug

--- xoa/src/com/test/Ctest.java
+++ xoa/src/com/test/Ctest.java
@@ -25,6 +25,7 @@ import com.xoa.service.workflow.flowtype.FlowRunService;
 import com.xoa.service.workflow.flowtype.FlowTypeService;
 import com.xoa.service.workflow.flowtype.impl.FlowRunPrcsServiceImpl;
 import com.xoa.util.dataSource.ContextHolder;
+import org.apache.poi.util.SystemOutLogger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -80,8 +81,11 @@ public class Ctest {
 //	@Resource
 //	private FlowRunPrcsService flowRunPrcsService;
 
+//	@Resource
+//	private FlowProcessService flowProcessService;
+
 	@Resource
-	private FlowProcessService flowProcessService;
+	private EmailService emailService;
 
 	@Test
 	public void test()throws  Exception{
@@ -127,8 +131,30 @@ public class Ctest {
 //		flowProcess.setTimeOutType("0");
 //		flowProcess.setWorkingdaysType("0");
 //		flowProcess.setTimeOutAttend("0");
-		FlowProcessList toJson =  flowProcessService.flowView(9);
-		System.out.println(JSON.toJSONStringWithDateFormat(toJson, "yyyy-MM-dd HH:mm:ss"));
+//		FlowProcessList toJson =  flowProcessService.flowView(9);
+//		System.out.println(JSON.toJSONStringWithDateFormat(toJson, "yyyy-MM-dd HH:mm:ss"));
+		EmailBodyModel emailBodyModel = new EmailBodyModel();
+		Map<String, Object> maps = new HashMap<String, Object>();
+		maps.put("emailId", 1279);
+		String fwRwEmail = emailService.queryByIdCss(maps, 1, 5, false, "xoa1001");
+		emailBodyModel.setContent("<p dir=\"ltr\">&#27979;&#35797;</p>\n" +
+				"                <p dir=\"ltr\">--&#21457;&#33258;&#25105;&#30340;&#24515;&#36890;&#36798;OA&#29256;--</p>");
+		emailBodyModel.setContent(emailBodyModel.getContent()+fwRwEmail);
+		String name =  emailBodyModel.getBodyId()+"";
+		System.out.println(name.equals("null"));
+
+		if("null".equals(name)) {
+			emailBodyModel.setSubject("Fw:草稿箱测试");
+			emailBodyModel.setAttachmentId("3638@1706_1653006878,3636@1706_1652995229,3637@1706_1652995238");
+			emailBodyModel.setAttachmentName("1496301636377.jpg*1496301624995.jpg*1496301625087.jpg*");
+			System.out.println(JSON.toJSONStringWithDateFormat(emailService.saveEmail(emailBodyModel), "yyyy-MM-dd HH:mm:ss"));
+		}else{
+			emailBodyModel.setSendFlag("0");
+			System.out.println(JSON.toJSONStringWithDateFormat(emailService.draftsSendEmail(emailBodyModel,new EmailModel()), "yyyy-MM-dd HH:mm:ss"));
+		}
+
+
+
 	}
 
 
